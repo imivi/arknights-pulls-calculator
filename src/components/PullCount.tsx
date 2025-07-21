f9b6cf80e51e5c782d88c051014d57d9f01ffd1f
imivi <contact@imivi.dev>
2025-07-21 20:24:24 +0200

    Fixed clear first day

---
+++ src/components/PullCount.tsx
@@ -0,0 +1,32 @@
+import { Day } from "../day"
+import { useDarkModeStore } from "../stores/useDarkModeStore"
+import s from "./PullCount.module.scss"
+import PullsMenu from "./PullsMenu"
+
+
+type Props = {
+    day: Day
+    even: boolean
+}
+
+export default function PullCount({ day, even }: Props) {
+
+    const { darkMode } = useDarkModeStore()
+
+    return (
+        <PullsMenu day={day} maxPulls={day.pullsAvailable}>
+            <div className={s.PullCount}>
+                {
+                    day.pullsSpent > 0 &&
+                    <span className={s.pulls_spent}>
+                        &nbsp;-{day.pullsSpent}
+                    </span>
+                }
+                <span className={s.pulls_available} data-dark={darkMode} data-even={even}>
+                    <strong>{day.pullsAvailable.toFixed()}</strong>
+                    <small>pulls</small>
+                </span>
+            </div>
+        </PullsMenu>
+    )
+}
